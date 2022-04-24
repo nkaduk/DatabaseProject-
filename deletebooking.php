477a87bd74714730c9506a733966689c9ec93374
Ramone928 <88847535+Ramone928@users.noreply.github.com>
2022-04-24 17:52:55 -0400

    Update deletebooking.php
    output for php file

--- deletebooking.php
+++ deletebooking.php
@@ -5,14 +5,25 @@
 
     <body>
         <?php
-        $conn = mysqli_connect("localhost", "nkaduk", "", "sleepy_inn");
-        if (!$conn){
+        $conn = new mysqli("localhost", "root", "", "sleepy_inn");
+        if ($conn->connect_error){
             echo "Connection not good";
         }
         $Account_ID = $_POST["Account_ID"];
         $Room_ID = $_POST["Room_ID"];
-        $sql = "delete from `booking_information` where Account_ID = '$Account_ID' and Room_ID = '$Room_ID'";
+        $sql = "delete from `booking_information` where Account_ID = '$Account_ID' and Hotel_Room_ID = '$Room_ID'";
         $result = $conn->query($sql);
+        $sql = "SELECT Last_Name, First_Name, Hotel_Room_ID, Date_Booked, Checked_In FROM `Booking_Information` natural join    `Customer`";
+        $result = $conn->query($sql);
+        $Table = $result->fetch_all(MYSQLI_ASSOC);
+        echo '<table id="Emplyees_table">';
+        echo '<tr><th>First Name</th><th>Last Name</th><th>Hotel_Room_ID</th><th>Date_Booked</th><th>checked_In</th></tr>';
+        foreach($Table as $row){
+        echo '<tr><td>'.$row['First_Name'].'</td><td>'.$row['Last_Name'].'</td><td>'.$row['Hotel_Room_ID'].'</td><td>'.$row['Date_Booked'].'</td><td>'.$row['Checked_In'].'</td></tr>';
+}
+
+        echo '</table>';
+
 
 
         ?>
