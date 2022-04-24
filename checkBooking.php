7557120a8e8f34512b0c14697f9714f937232a62
nkaduk <nkaduk@kent.edu>
2022-04-24 16:35:50 -0400

    PHP update

--- checkBooking.php
+++ checkBooking.php
@@ -1,20 +1,20 @@
 <html>
 <head>
-    <title> Check Booking <\title>
-<\head>
+    <title> Check Booking </title>
+</head>
 
     <body>
         <?php
-        $conn = mysqli_connect('localhost', 'nkaduk', 'BP2271YwooooodIcsAPWIllI', 'sleepy inn');
+        $conn = mysqli_connect("localhost", "nkaduk", "", "sleepy_inn");
         if (!$conn){
             echo "Connection not good";
         }
-        $Account_ID = $_POST[‘Account_ID’];
-        $sql = "SELECT LastName, FirstName, Hotel-Room_ID, DateBooked, CheckedIn FROM `Booking Information` natural join    `Customer` WHERE Account_ID = '$Account_ID'";
+        $Account_ID = $_POST["Account_ID"];
+        $sql = "SELECT Last_Name, First_Name, Hotel_Room_ID, Date_Booked, Checked_In FROM `Booking_Information` natural join    `Customer` WHERE Account_ID = '$Account_ID'";
         $result = $conn->query($sql);
 
 
         ?>
         
-    <\body>
-<\html>
+    </body>
+</html>
--- deletebooking.php
+++ deletebooking.php
@@ -1,21 +1,21 @@
 <html>
 <head>
-    <title> Check Booking <\title>
-<\head>
+    <title> Check Booking </title>
+</head>
 
     <body>
         <?php
-        $conn = mysqli_connect('localhost', 'nkaduk', 'BP2271YwooooodIcsAPWIllI', 'sleepy inn');
+        $conn = mysqli_connect("localhost", "nkaduk", "", "sleepy_inn");
         if (!$conn){
             echo "Connection not good";
         }
-        $Account_ID = $_POST[‘Account_ID’];
-        $Room_ID = $_POST['Room_ID'];
-        $sql = "delete from `booking information` where Account_ID = '$Account_ID' and Room_ID = '$Room_ID'";
+        $Account_ID = $_POST["Account_ID"];
+        $Room_ID = $_POST["Room_ID"];
+        $sql = "delete from `booking_information` where Account_ID = '$Account_ID' and Room_ID = '$Room_ID'";
         $result = $conn->query($sql);
 
 
         ?>
         
-    <\body>
-<\html>
+    </body>
+</html>
