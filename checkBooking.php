cf4aa4f9a23641ec2367beaa12346e817aa1df1b
Ramone928 <88847535+Ramone928@users.noreply.github.com>
2022-04-24 17:54:04 -0400

    Update checkBooking.php

--- checkBooking.php
+++ checkBooking.php
@@ -5,17 +5,24 @@
 
     <body>
         <?php
-        $conn = mysqli_connect("localhost", "root", "", "sleepy_inn");
-        if (!$conn){
+        $conn = new mysqli("localhost", "root", "", "sleepy_inn");
+        if ($conn->connect_error){
             echo "Connection not good";
         }
         $Account_ID = $_POST["Account_ID"];
         $sql = "SELECT Last_Name, First_Name, Hotel_Room_ID, Date_Booked, Checked_In FROM `Booking_Information` natural join    `Customer` WHERE Account_ID = '$Account_ID'";
         $result = $conn->query($sql);
+        $Table = $result->fetch_all(MYSQLI_ASSOC);
+        echo '<table id="Emplyees_table">';
+        echo '<tr><th>First Name</th><th>Last Name</th><th>Hotel_Room_ID</th><th>Date_Booked</th><th>checked_In</th></tr>';
+        foreach($Table as $row){
+        echo '<tr><td>'.$row['First_Name'].'</td><td>'.$row['Last_Name'].'</td><td>'.$row['Hotel_Room_ID'].'</td><td>'.$row['Date_Booked'].'</td><td>'.$row['Checked_In'].'</td></tr>';
+}
 
-        while ($row = $result->fetch_assoc()) {
-            printf("%s %s %s %s (%s)\n", $row["Last_Name"], $row["First_Name"], $row["Hotel_Room_ID"], $row["Date_Booked"], $row["Checked_In"]);
-        }
+        echo '</table>';
+
+
+
         ?>
         
     </body>
