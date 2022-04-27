6a2e7021c05d8ae8453379191de89e7dec7f1b78
nkaduk <nkaduk@kent.edu>
2022-04-27 15:02:31 -0400

    Finished Insertion booking (could use another insertion page for customer accounts)

--- insert.php
+++ insert.php
@@ -9,23 +9,31 @@
      if (!$conn){
         echo "Connection not good";
     }
+
     $Account_ID = $_POST["Account_ID"];
     $Hotel_Room_ID = $_POST["HRID"];
     $Hotel_ID = $_POST["Hotel_ID"];
     $Date_Booked = $_POST["Date_Booked"];
+
+    /*$sql = "CREATE TRIGGER `occ_update` before insert on `Booking_Information`
+    BEGIN
+    UPDATE `Room_Availability` set Availability = 'Occupied' where Hotel_Room_ID = '$Hotel_Room_ID'; 
+    END;";*/
     
-    $sql = "insert into `Booking_Information` values ('$Account_ID', '$Hotel_Room_ID', '$Hotel_ID', '$Date_Booked', 'N')";
+    
+    $sql = "update `Room_Availability` set Availability = 'Occupied' where Hotel_Room_ID = '$Hotel_Room_ID'";
+    $conn ->query($sql);
+
+
+    
+    $sql = "insert into `Booking_Information` values ('$Hotel_Room_ID', '$Account_ID', '$Hotel_ID', '$Date_Booked', 'N')";
     if ($conn->query($sql) === TRUE) {
   echo "Your room has been booked!";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$sql = "CREATE TRIGGER `occ_update` after insert on `Booking_Information`
-    BEGIN
-    UPDATE `Room_Availability` set Availability = 'Occupied' where Hotel_Room_ID = '$Hotel_Room_ID'; 
-    END;";
-    
     ?>
+    
 
 </body>
