f8be9ad8b533d5c27640d328025aef7a8a43e244
nkaduk <nkaduk@kent.edu>
2022-04-27 09:15:38 -0400

    Fixed Booking.php, need help on insertBooking.html and insert.php

--- booking.php
+++ booking.php
@@ -18,9 +18,28 @@
 
 }
 
+if ($Location == "Sleepy Inn near Highway 89"){
+$hotelNameInt = 1001;
+} elseif ($Location == "Sleepy Inn of Washington DC"){
+$hotelNameInt = 1002;
+}else{
+  $hotelNameInt = 1003;
+}
 
 
-    $sql = "SELECT Name FROM hotel_location WHERE Name = '$Location'";
+
+    $sql = "select Name, Hotel_ID, Room_Number, Room_Type from Hotel_Information natural join Room_Availability where Room_Type = '$Room' and Hotel_ID = '$hotelNameInt' and Availability = 'Available'";
+
+    $result = $conn->query($sql);
+    $Table = $result->fetch_all(MYSQLI_ASSOC);
+    echo '<table id="Emplyees_table">';
+    echo '<tr><th>Name</th><th>Hotel_ID</th><th>Room_Number</th><th>Room_Type</th></tr>';
+    foreach($Table as $row){
+    echo '<tr><td>'.$row['Name'].'</td><td>'.$row['Hotel_ID'].'</td><td>'.$row['Room_Number'].'</td><td>'.$row['Room_Type'].'</td></tr>';
+}
+
+    echo '</table>';
+    /*$sql = "SELECT Name FROM hotel_location WHERE Name = '$Location'";
     $result = $conn->query($sql);
     $sql2 = "SELECT DISTINCT Room_Type FROM room_availability WHERE Room_type = '$Room' and Availability = 'Available'";
     $result2 = $conn->query($sql2);
@@ -41,7 +60,7 @@
 
     echo '</table>';
     echo 'If a room is avaible, call here: 330-xxx-xxxx';
-    
+    */
     ?>
 
 </body>
